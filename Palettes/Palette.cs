15dfa317b62a9d56f0e0a9db2dbcc8715bf52605
Luis Llamas <luisllamas@hotmail.com>
2022-07-08 19:05:25 +0200

    Added ColorPalettes

---
+++ Palettes/Palette.cs
@@ -0,0 +1,22 @@
+﻿public class ColorPalette
+{
+    public static (float r, float g, float b) GetColorFloat(float index, (int r, int g, int b)[] palette, bool reversed = false)
+    {
+        if (reversed) index = 1.0f - index;
+        var i = (int)(index * 255);
+        
+        float r = (float)palette[i].r / 255f;
+        float g = (float)palette[i].g / 255f;
+        float b = (float)palette[i].b / 255f;
+
+        return (r, g, b);
+    }
+
+    public static (int r, int g, int b) GetColorInt(float index, (int r, int g, int b)[] palette, bool reversed = false)
+    {
+        if (reversed) index = 1.0f - index;
+
+        var i = (int)(index * 255);
+        return palette[i];
+    }
+};
